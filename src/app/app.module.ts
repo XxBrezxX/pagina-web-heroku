9e904e3dcd0ba05e36cd9226faeaab04f9674074
XxBrezxX <77246886+XxBrezxX@users.noreply.github.com>
2021-11-07 19:31:01 -0600

    V4.1.0
    Se agregó la biografía de Vigenere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { BiographyComponent } from './biography/biography.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    BiographyComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BiographyComponent } from './biography/biography.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'aboutMe', component: AboutMeComponent}
+    {path: 'aboutMe', component: AboutMeComponent},
+    {path: 'biography', component: BiographyComponent}
 ];
 
 export const appRoutingProviders: any[]=[];
